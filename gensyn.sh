86cdc65c76a6c8e6a3bc753e9a0862a8cf6a7656
noderguru <110336041+noderguru@users.noreply.github.com>
2025-05-04 08:07:46 +0300

    Update gensyn.sh

--- gensyn.sh
+++ gensyn.sh
@@ -47,7 +47,7 @@ if [ -f "$SWARM_DIR/swarm.pem" ]; then
     done
 else
     echo -e "${BOLD}${YELLOW}[✓] No existing swarm.pem found. Cloning repository...${NC}"
-    cd $HOME && [ -d rl-swarm ] && rm -rf rl-swarm; git clone https://github.com/zunxbt/rl-swarm.git > /dev/null 2>&1
+    cd $HOME && [ -d rl-swarm ] && rm -rf rl-swarm; git clone https://github.com/noderguru/rl-swarm.git > /dev/null 2>&1
 fi
 
 cd rl-swarm || { echo -e "${BOLD}${RED}[✗] Failed to enter rl-swarm directory. Exiting.${NC}"; exit 1; }
